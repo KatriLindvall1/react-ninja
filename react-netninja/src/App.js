556b38078fb0f5687686f8cab637b74b06bb0c43
Katri Lindvall <katri@stealth.black>
2021-01-04 13:46:05 +0200

    props

--- react-netninja/src/App.js
+++ react-netninja/src/App.js
@@ -1,7 +1,11 @@
+import Ninjas from "./Ninjas";
+
 function App() {
   return (
     <div className="App">
       <h1>React app!</h1>
+      <p>Welcome</p>
+      <Ninjas name="Ryu" age="25" belt="black" />
     </div>
   );
 }
