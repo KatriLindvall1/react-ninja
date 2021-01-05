257fe667dac58f049c3e9d4ea383e08edf248d4c
Katri Lindvall <katri@stealth.black>
2021-01-05 09:44:57 +0200

    delete todos

--- react-netninja/src/App.js
+++ react-netninja/src/App.js
@@ -1,45 +1,25 @@
 import React, { Component } from "react";
-import Ninjas from "./Ninjas";
-import AddNinja from "./AddNinja";
-
+import Todos from "./Todos";
 class App extends Component {
   state = {
-    ninjas: [
-      { name: "Ryu", age: 30, belt: "black", id: 1 },
-      { name: "Mia", age: 20, belt: "green", id: 2 },
-      { name: "Juha", age: 25, belt: "pink", id: 3 },
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" },
     ],
   };
-  addNinja = (ninja) => {
-    ninja.id = Math.random();
-    let ninjas = [...this.state.ninjas, ninja];
-    this.setState({
-      ninjas: ninjas,
-    });
-  };
-  deleteNinja = (id) => {
-    let ninjas = this.state.ninjas.filter((ninja) => {
-      return ninja.id !== id;
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
     });
     this.setState({
-      ninjas: ninjas,
+      todos: todos,
     });
   };
-  componentDidMount() {
-    console.log("component mounted");
-  }
-  componentDidUpdate(prevProps, prevState) {
-    console.log("component updated");
-    console.log(prevProps);
-    console.log(prevState);
-  }
   render() {
     return (
-      <div className="App">
-        <h1>React app!</h1>
-        <p>Welcome</p>
-        <Ninjas ninjas={this.state.ninjas} deleteNinja={this.deleteNinja} />
-        <AddNinja addNinja={this.addNinja} />
+      <div className="todo-app container">
+        <h1 className="center blue-text"> Todo´s</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
